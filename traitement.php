b8ceff298d7785424e09412b2d23f4743f1c1540
Extreeeme <martindcontact@gmail.com>
2017-02-12 13:26:15 +0100

    Ajout des droits d'administrateurs

--- traitement.php
+++ traitement.php
@@ -2,14 +2,24 @@
 	session_start();
 	$bdd = new PDO('mysql:host=localhost;dbname=test', 'root', '' ); // Connexion à la base de données
 	$pseudo = $_SESSION['pseudo']; // pseudo saisi par l'utilisateur
-	$message = $_POST['message']; // message saisi par l'utilisateur
+	$message = htmlspecialchars($_POST['message']); // message saisi par l'utilisateur
 
 	// Prepare la requete qui est : insert dans le tab 'chat' (catégorie pseudo et message)
 	// des valeurs encore non définies ! 
+	$messageSplit = explode(" ", $message);
+	if($messageSplit[0] == "/r" AND ($_SESSION["pseudo"] == "Martin"  OR $_SESSION["pseudo"] == "Aymeric")	 ){
+		$bdd->exec('DELETE from chat WHERE id=$messageSplit[1]');
+		$req = $bdd->prepare('DELETE from chat WHERE id='.$messageSplit[1].'');
+		$req->execute(array(
+		'id' => $messageSplit[1]));
+	}else{
+
 	$envoi = $bdd->prepare('INSERT INTO chat(pseudo, message) VALUES(:pseudo, :message) ');
 	
 	// Execute la requete, en précisant que les valeurs qui étaient inconnues sont renseignées
 	$envoi->execute(array('pseudo' => $pseudo, 'message' => $message));
+	}
+	
 
 	header('Location: index.php');
 	
